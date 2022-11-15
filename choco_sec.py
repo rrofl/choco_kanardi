c46525e969775960cf6e6b6d9990b4d10ec02790
rrofl <74879024+rrofl@users.noreply.github.com>
2022-11-15 21:09:12 +0100

    Update choco_sec.py

--- choco_sec.py
+++ choco_sec.py
@@ -7,6 +7,7 @@ a = 60
 
 def button(*args):
     values = [(prog, var.get()) for prog, var in data.items()]
+    print(values)
 
 window = Tk()
 window.title("Chocolatey")
@@ -15,11 +16,11 @@ window.configure(bg = "#088F8F")
 
 for prog in do_instalacji:
     var = IntVar()
-    Checkbutton(window, text = prog, variable=var, onvalue=True, offvalue= False, bg = "#088F8F", font=("", 16)).place(x = 110, y = a)
+    Checkbutton(window, text=prog, variable=var, onvalue=1, offvalue=0, bg="#088F8F", font=("", 16)).place(x=110, y=a)
     data[prog] = var
     a = a + 40
 
-programy = Label(window, text="Wybierz programy do instalacji: ", bg = "#088F8F", font=("", 19, 'bold')).place(x = 110, y = 20)
-przycisk = Button(window, text="Zainstaluj programy!", command=button, font=("", 19,)).place(x = 170, y = 300)
+programy = Label(window, text="Wybierz programy do instalacji: ", bg="#088F8F", font=("", 19, 'bold')).place(x=110, y=20)
+przycisk = Button(window, text="Zainstaluj programy!", command=button, font=("", 19,)).place(x=170,y =300)
 
 window.mainloop()
