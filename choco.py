0223f8bd5808a972cf8522668ab44e773f562784
rrofl <74879024+rrofl@users.noreply.github.com>
2022-11-15 20:25:52 +0100

    updacik maly

--- choco.py
+++ choco.py
@@ -45,3 +45,4 @@ thunderbird = Checkbutton(window, text='7 Zip', variable=var6, onvalue=True, off
 przycisk = Button(window, text="Zainstaluj programy!", command=button, font=("", 19,)).place(x = 170, y = 300)
 
 window.mainloop()
+print('siema')
