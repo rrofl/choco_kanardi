64440068c394adea1c1775914ddfa255597cb752
rrofl <74879024+rrofl@users.noreply.github.com>
2022-11-16 13:13:38 +0100

    kanardi 13;13

--- choco_sec.py
+++ choco_sec.py
@@ -9,12 +9,8 @@ b = 0
 
 def button(*args):
     values = [(prog, var.get()) for prog, var in data.items()]
+    print(true_or_false[0])
     print(values)
-    for item in links:
-        if var.get() in data.keys() == 1:
-            print('jd')
-            print(item)
-            b += 1
 
 window = Tk()
 window.title("Chocolatey")
@@ -25,8 +21,8 @@ for prog in do_instalacji:
     var = IntVar()
     Checkbutton(window, text=prog, variable=var, onvalue=1, offvalue=0, bg="#344955", font=("", 16)).place(x=110, y=a)
     data[prog] = var
-    a = a + 40
-
+    true_or_false = [(var.get() for var in data.items())]
+    a += 40
 
 programy = Label(window, text="Wybierz programy do instalacji: ", bg="#344955", font=("", 19, 'bold')).place(x=110, y=20)
 przycisk = Button(window, text="Zainstaluj programy!", command=button, font=("", 19,)).place(x=170,y =300)
