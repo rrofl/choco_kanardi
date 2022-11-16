2c4e001c1e9547b8bde5f8f60f55a2f50a4e1db1
rrofl <74879024+rrofl@users.noreply.github.com>
2022-11-16 15:57:03 +0100

    Update choco.py

--- choco.py
+++ choco.py
@@ -1,42 +1,71 @@
 from tkinter import *
 import os
 
-do_instalacji = ['Google Chrome', 'Adobe Reader', '7 Zip', 'VLC', 'Slack', 'Revit']
-links = ['choco install googlechrome -y',
- 'choco install adobereader -y', 
- 'choco install 7zip.install -y', 
- 'choco install vlc -y', 
- 'choco install slack -y',
- 'start revit.exe'
- ]
+programy = ['Google Chrome', 'Adobe Reader', '7 Zip', 'VLC', 'Slack', 'Revit']
+zestawy = ['Optident', 'Rexer', 'Arkana', 'Invent', 'DTA', 'VM', 'Netkable', 'HMMH']
+odnosniki = [
+    'choco install googlechrome -y',
+    'choco install adobereader -y', 
+    'choco install 7zip.install -y', 
+    'choco install vlc -y', 
+    'choco install slack -y',
+    'start revit.exe'
+            ]
+odnosniki_zestawy = {
+    'optident' : '',
+    'rexer' : '',
+    'arkana' : '',
+    'invent' : '',
+    'dta' : '',
+    'vm' : '',
+    'netkable' : '',
+    'hmmh' : ''
+                    }
+
 data = {}
+width = 600
+length = 500
 a = 60
 b = 0
+c = 60
 
 def button(*args):
     values = [(prog, var.get()) for prog, var in data.items()]
+    values2 = [(prog, var.get()) for prog, var in data.items()]
     x = 0
-    for q in do_instalacji:
+    for q in programy:
         if values[x][1] == 1:
-            os.system(f'{links[x]}')
+            os.system(f'{odnosniki[x]}')
             x+=1
         else:
             print('brak')
             x+=1
-    #print(values)
+    for e in zestawy:
+        if values2[x][1] == 1:
+            os.system(f'{odnosniki[x]}')
+            x+=1
+        else:
+            print('brak')
+            x+=1
 
 window = Tk()
 window.title("Chocolatey")
-window.geometry("600x400") 
+window.geometry(f"{width}x{length}") 
 window.configure(bg = "#344955")
 
-for prog in do_instalacji:
+for prog in programy:
     var = IntVar()
-    Checkbutton(window, text=prog, variable=var, onvalue=1, offvalue=0, bg="#344955", font=("", 16)).place(x=110, y=a)
+    Checkbutton(window, text=prog, variable=var, onvalue=1, offvalue=0, bg="#344955", font=("", 16)).place(x=width/20, y=a)
     data[prog] = var
     a += 40
+for zest in zestawy:
+    var2 = IntVar()
+    Checkbutton(window, text=zest, variable=var2, onvalue=1, offvalue=0, bg="#344955", font=("", 16)).place(x=width/1.5, y=c)
+    data[prog] = var2
+    c += 40
 
-programy = Label(window, text="Wybierz programy do instalacji: ", bg="#344955", font=("", 19, 'bold')).place(x=110, y=20)
-przycisk = Button(window, text="Zainstaluj programy!", command=button, font=("", 19,)).place(x=170,y =300)
+programy = Label(window, text="Wybierz programy/zestaw do instalacji: ", bg="#344955", font=("", 19, 'bold')).place(x=width/9, y=length/30)
+przycisk = Button(window, text="Zainstaluj!", command=button, font=("", 19,)).place(x=width/2.6,y=length-60)
 
+window.resizable(0,0)
 window.mainloop()
