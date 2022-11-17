b9f410a2554132410be4fc2f8f980ef218e0abdb
rrofl <74879024+rrofl@users.noreply.github.com>
2022-11-17 19:51:16 +0100

    def 17.11.2022

--- choco.py
+++ choco.py
@@ -10,7 +10,6 @@ odnosniki = ['choco install googlechrome -y',
  'start revit.exe'
  ]
 
-"""
 zestawy = ['Optident', 'Rexer', 'Arkana', 'Invent', 'DTA', 'VM', 'Netkable', 'HMMH']
 odnosniki_zestawy = {
     'optident' : '',
@@ -22,10 +21,9 @@ odnosniki_zestawy = {
     'netkable' : '',
     'hmmh' : ''
                     }
-"""
 
 data = {}
-#data2 = {}
+data2 = {}
 width = 600
 length = 500
 a = 80
@@ -34,9 +32,9 @@ c = 80
 
 def button(*args):
     values = [(prog, var.get()) for prog, var in data.items()]
-    #values2 = [(zest, var2.get()) for zest, var2 in data2.items()]
+    values2 = [(zest, var2.get()) for zest, var2 in data2.items()]
     x = 0
-    #y = 0
+    y = 0
     for q in customowe:
         if values[x][1] == 1:
             os.system(f'{odnosniki[x]}')
@@ -44,7 +42,6 @@ def button(*args):
         else:
             print('brak')
             x+=1
-    """
     for p in zestawy:
         if values2[y][1] == 1:
             os.system(f'{odnosniki_zestawy[y]}')
@@ -52,7 +49,6 @@ def button(*args):
         else:
             print('brak')
             y+=1
-    """
 
 window = Tk()
 window.title("Chocolatey")
@@ -63,14 +59,11 @@ for prog in customowe:
     Checkbutton(window, text=prog, variable=var, onvalue=1, offvalue=0, text_font=("", 18)).place(x=width/20, y=a)
     data[prog] = var
     a += 40
-
-"""
 for zest in zestawy:
     var2 = IntVar()
-    CTkCheckBox(window, text=zest, variable=var2, onvalue=1, offvalue=0, text_font=("", 18)).place(x=width/1.5, y=c)
+    Checkbutton(window, text=zest, variable=var2, onvalue=1, offvalue=0, text_font=("", 18)).place(x=width/1.5, y=c)
     data[prog] = var2
     c += 40
-"""
 
 programy = Label(window, text="Wybierz programy/zestaw do instalacji: ", text_font=("", 19, 'bold')).place(x=width/9, y=length/30)
 przycisk = Button(window, text="Zainstaluj!", command=button, text_font=("", 19)).place(x=width/2.6,y=length-60)
