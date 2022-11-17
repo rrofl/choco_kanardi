ee338a5fdbf92526b0e36fbea17b9c0bdda76102
rrofl <74879024+rrofl@users.noreply.github.com>
2022-11-17 20:57:03 +0100

    Update choco.py

--- choco.py
+++ choco.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import os
+import multiprocessing
+multiprocessing.freeze_support()
 
 customowe = ['Google Chrome', 'Adobe Reader', '7 Zip', 'VLC', 'Slack', 'Revit']
 odnosniki = ['choco install googlechrome -y',
@@ -9,14 +11,14 @@ odnosniki = ['choco install googlechrome -y',
  'choco install slack -y',
  'start revit.exe'
  ]
-
+"""
 zestawy = ['Optident', 'Rexer', 'Arkana', 'Invent', 'DTA', 'VM', 'Netkable', 'HMMH']
 odnosniki_zestawy = ['choco install googlechrome -y',
  'choco install 7zip.install -y'
  ]
-
+"""
 data = {}
-data2 = {}
+#data2 = {}
 width = 600
 length = 500
 a = 80
@@ -42,11 +44,11 @@ for prog in customowe:
     Checkbutton(window, text=prog, variable=var, onvalue=1, offvalue=0, font=("", 18)).place(x=width/20, y=a)
     data[prog] = var
     a += 40
-
+"""
 def button_multi(*args):
-    values2 = [(zest, var2.get()) for zest, var2 in data2.items()]
+    values2 = [(zest, var.get()) for zest, var in data2.items()]
     y = 0
-    for p in zestawy:
+    for u in zestawy:
         if values2[y][1] == 1:
             os.system(f'{odnosniki_zestawy[y]}')
             y+=1
@@ -54,15 +56,14 @@ def button_multi(*args):
             print('brak zestawow')
             y+=1
 for zest in zestawy:
-    var2 = IntVar()
-    Checkbutton(window, text=zest, variable=var2, onvalue=1, offvalue=0, font=("", 18)).place(x=width/1.5, y=c)
-    data2[zest] = var2
+    var = IntVar()
+    Checkbutton(window, text=zest, variable=var, onvalue=1, offvalue=0, font=("", 18)).place(x=width/1.5, y=c)
+    data2[zest] = var
     c += 40
-
-
+"""
 programy = Label(window, text="Wybierz programy/zestaw do instalacji: ", font=("", 19, 'bold')).place(x=width/9, y=length/30)
 przycisk_single = Button(window, text="Zainstaluj programy!", command=button_single, font=("", 19)).place(x=width/30,y=length-60)
-przycisk_multi = Button(window, text="Zainstaluj zestaw!", command=button_multi, font=("", 19)).place(x=width/1.7,y=length-60)
+#przycisk_multi = Button(window, text="Zainstaluj zestaw!", command=button_multi, font=("", 19)).place(x=width/1.7,y=length-60)
 
 window.resizable(0,0)
 window.mainloop()
